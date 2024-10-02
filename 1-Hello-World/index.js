1ddfe821d10653588b966c219609cd3c7822a85c
tancrescens <127433377+tancrescens@users.noreply.github.com>
2024-10-02 07:11:47 +0000

    completed challenge

--- 1-Hello-World/index.js
+++ 1-Hello-World/index.js
@@ -1,1 +1,9 @@
+window.addEventListener("DOMContentLoaded", async function(){
+    let url = `https://4geeksacademy.github.io/exercise-assets/txt/hello.txt`
+    let response = await axios.get(url);
 
+    let content = document.querySelector("#content");
+    console.log(response.data)
+    let html = `${response.data}`
+    content.innerHTML = html
+})
